09194389ce23423e2f71bace63b23ef5a20ed04f
ion22666 <ionmocanu2266@gmail.com>
2023-08-22 11:54:21 +0300

    added base path url for deploy only

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
     transpileDependencies: true,
+
+    publicPath: process.env.NODE_ENV === 'production'? 'https://ion22666.github.io/alsodev-tasks/': '/',
 });
